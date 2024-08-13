05cb0e8b551ce414da7e85e1eab02a07586f80ea
HuyNguyen <duchuy1912.hn@gmail.com>
2024-08-13 22:29:34 +0700

    Update Db url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 8000 ;
 const app = express();
 
 // 1) Configure mongoose
-const db = "mongodb://127.0.0.1:27017/OnlineStore"; // OnlineStore: database name
+const db = "mongodb+srv://huyndgch221065:8b5VJiXjv2fTKvSa@cluster0.8w7eq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
 mongoose.connect(db, {
   useNewUrlParser: true,
   useUnifiedTopology: true
